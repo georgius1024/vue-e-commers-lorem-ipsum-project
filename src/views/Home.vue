44036671b077eae797670431b5f3efe8b0f25c66
Timofeev Yurii <georgius@ural.ru>
2020-08-15 23:07:44 +0500

    remover load delay

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container v-if="dataLoaded">
+  <v-container>
     <h1 class="display-4 mb-5">Project-name</h1>
     <div class="text-center my-6">
       <img :src="require('../assets/logo.svg')" width="120" alt="logo" />
@@ -80,15 +80,5 @@
 <script>
 export default {
   name: 'Home',
-  data() {
-    return {
-      dataLoaded: false
-    }
-  },
-  mounted() {
-    setTimeout(() => {
-      this.dataLoaded = true
-    }, 200)
-  }
 }
 </script>
