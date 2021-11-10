a5bf3cdf5c90ed5489bd8e2610944de5c32a88dc
georgius1024 <georgius@ural.ru>
2021-11-10 10:35:32 +0500

    removed prerendering

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,6 @@
-const services = require('./src/assets/services.json').map(e => e.id)
 module.exports = {
   transpileDependencies: ['vuetify'],
 
   pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: ['/', '/prices/', '/contacts/', '/services/', ...services.map(id => `/services/${id}/`)],
-      useRenderEvent: true,
-      headless: false,
-      onlyProduction: true
-    }
   }
 }
